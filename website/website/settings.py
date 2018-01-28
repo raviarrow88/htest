e1cc16c6485454cfe94c63e9bbb32faabe7b63bb
Ravi Teja <raviarrow88@gmail.com>
2018-01-28 17:52:27 +0530

    static files added

--- website/website/settings.py
+++ website/website/settings.py
@@ -123,3 +123,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
